a71530725d5b3a198e1150c4f75dacc47edaa9d2
Sachin <leavethesq@gmail.com>
2024-09-15 22:08:52 +0530

    half-way

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,35 @@
   <h1>HATTORI THE NINJA</h1>
   <h3>REACTION GAME</h3>
   <p>Let's see you're worthy</p>
+
+  <button @click="start" :disabled="isPlaying" >Start</button>
+
+  <UseBlock v-if="isPlaying" :delay="delay" />
+
 </template>
 
 <script>
 
+import UseBlock from './components/useBlock.vue';
+
+
 export default {
   name: 'App',
-  components: { }
+  components: {
+    UseBlock
+  },
+  data(){
+    return{
+      isPlaying:false,
+      delay:null
+    }
+  },
+  methods:{
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+    }
+  },
 }
 </script>
 
